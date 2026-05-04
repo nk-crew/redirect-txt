37f799938212ed9cabe7ede77edb4992346241e0
Fellan-91 <alexey.fellan@gmail.com>
2026-05-04 17:38:39 +0300

    Build WordPress 7 compatibility assets

--- build/admin.asset.php
+++ build/admin.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e1411be9dc8b2ad37bef');
+<?php return array('dependencies' => array('lodash', 'react', 'react-dom', 'wp-api-fetch', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '8ed8e637fda838392729');
